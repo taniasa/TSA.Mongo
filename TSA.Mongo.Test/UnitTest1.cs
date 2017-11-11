8506f9cb0b30ef8803650d0ebfa9756aa941ca51
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2017-11-11 00:03:46 -0200

    Add projeto

---
+++ TSA.Mongo.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TSA.Mongo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
